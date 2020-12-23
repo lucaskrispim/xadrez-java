085fe072429851e79e16e7fc818f748a0e62ce27
lucaskrispim <lucaskrispim@hotmail.com>
2020-12-23 20:17:13 -0300

    Feature: print board

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,12 +1,13 @@
 package aplication;
 
-import boardgame.Position;
+import boardgame.Board;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Position pos = new Position(3,5);
-		System.out.println(pos);
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 
 	}
 
